c79957791afda511be6e738d9cbdd52eca645b64
emre.altun <emre.altun@trendyol.com>
2024-06-01 16:57:48 +0300

    Add EntityGraph example

--- src/main/java/com/trendyol/trainingapi/infrastracture/persistence/repository/OrderRepository.java
+++ src/main/java/com/trendyol/trainingapi/infrastracture/persistence/repository/OrderRepository.java
@@ -3,6 +3,7 @@ package com.trendyol.trainingapi.infrastracture.persistence.repository;
 import com.trendyol.trainingapi.infrastracture.persistence.entity.OrderEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,10 +13,14 @@ import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
 
+    @EntityGraph(attributePaths = {"fulfilmentAddress"})
     List<OrderEntity> findByUser_Id(Long userId);
 
     @Query("select o from OrderEntity o join fetch o.user join fetch o.fulfilmentAddress where o.id = :id")
     Optional<OrderEntity> findOrderByJpql(@Param("id") Long id);
+
+    @EntityGraph(attributePaths = {"user", "fulfilmentAddress"})
+    Optional<OrderEntity> findOrderEntityById(Long id);
 
     @Query("select o from OrderEntity o join fetch o.user join fetch o.fulfilmentAddress")
    // @Query("select o from Order o join fetch o.user join fetch o.fulfilmentAddress join fetch o.orderItems")
