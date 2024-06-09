0b83384116d06768c6588693b4da0927e03bb39d
emre.altun <emre.altun@trendyol.com>
2024-06-09 07:49:39 +0300

    Add hikari config

--- infrastructure/src/main/java/com/trendyol/training/infrastructure/persistence/repository/UserSpecification.java
+++ infrastructure/src/main/java/com/trendyol/training/infrastructure/persistence/repository/UserSpecification.java
@@ -1,6 +1,5 @@
 package com.trendyol.training.infrastructure.persistence.repository;
 
-import com.trendyol.training.infrastructure.common.enm.SearchOperation;
 import com.trendyol.training.infrastructure.rest.request.SearchCriteria;
 import com.trendyol.training.infrastructure.persistence.entity.UserEntity;
 import org.springframework.data.jpa.domain.Specification;
@@ -9,15 +8,15 @@ public class UserSpecification {
     public static Specification<UserEntity> withCriteria(SearchCriteria criteria) {
         return (root, query, builder) -> {
             switch (criteria.getOperation()) {
-                case SearchOperation.GREATER_THAN -> {
+                case GREATER_THAN -> {
                     return builder.greaterThanOrEqualTo(
                             root.get(criteria.getKey()), criteria.getValue().toString());
                 }
-                case SearchOperation.LESS_THAN -> {
+                case LESS_THAN -> {
                     return builder.lessThanOrEqualTo(
                             root.get(criteria.getKey()), criteria.getValue().toString());
                 }
-                case SearchOperation.EQUALS -> {
+                case EQUALS -> {
                     if (root.get(criteria.getKey()).getJavaType() == String.class) {
                         return builder.like(
                                 root.get(criteria.getKey()), "%" + criteria.getValue() + "%");
